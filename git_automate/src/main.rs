a802ce2b66ffec73b024dfb0aac8c84c1abbf79a
senyoudev <younesmeskafe2018@gmail.com>
2024-04-12 02:19:44 +0000

    add more checks & default values for remote and branch

--- git_automate/src/main.rs
+++ git_automate/src/main.rs
@@ -1,13 +1,7 @@
-use std::process::{Command, exit};
 use std::env::args;
-use std::io::Write;
+use std::process::{exit, Command};
 
-
-fn update_commit_push(
-    commit_message: &str,
-    remote: &str,
-    branch: &str
-) {
+fn update_commit_push(commit_message: &str, remote: &str, branch: &str) {
     // Create a new command to run /git add ./
     let add_command = Command::new("git")
         .arg("add")
@@ -17,9 +11,10 @@ fn update_commit_push(
 
     // Check if the command was successful
     if !add_command.status.success() {
-        eprintln!("Failed to add files : {}",
-        String::from_utf8_lossy(&add_command.stderr) // This will converts the error to a string and print it
-    );
+        eprintln!(
+            "Failed to add files : {}",
+            String::from_utf8_lossy(&add_command.stderr) // This will converts the error to a string and print it
+        );
         exit(1);
     }
 
@@ -33,7 +28,10 @@ fn update_commit_push(
 
     // Check if the command was successful
     if !commit_command.status.success() {
-        eprintln!("Failed to commit files: {}", String::from_utf8_lossy(&commit_command.stderr));
+        eprintln!(
+            "Failed to commit files: {}",
+            String::from_utf8_lossy(&commit_command.stderr)
+        );
         exit(1);
     }
 
@@ -47,28 +45,51 @@ fn update_commit_push(
 
     // Check if the command was successful
     if !push_command.status.success() {
-        eprintln!("Failed to push files: {}", String::from_utf8_lossy(&push_command.stderr));
+        eprintln!(
+            "Failed to push files: {}",
+            String::from_utf8_lossy(&push_command.stderr)
+        );
         exit(1);
     }
 
     println!("Successfully pushed changes to remote repository");
-
 }
 
 fn main() {
     // params should be passed in the cli as arguments and we get them using std::env::args()
     let args: Vec<String> = args().collect();
+    let default_remote = String::from("origin");
+    let default_branch = String::from("main");
 
-    // Check if the number of arguments is correct
-    if args.len() != 4 {
-        eprintln!("Usage: git-auto-push <commit_message> <remote> <branch>");
+ 
+    let commit_message = &args[1];
+    let remote = args.get(2).unwrap_or(&default_remote);
+    let branch = args.get(3).unwrap_or(&default_branch);
+
+    // Do the validation of the arguments here
+    if commit_message.is_empty() {
+        eprintln!("Commit message cannot be empty");
         exit(1);
     }
 
-    let commit_message = &args[1];
-    let remote = &args[2];
-    let branch = &args[3];
+    // check if the remote or branch name contain invalid characters
+    if !check_for_invalid_characters(remote) {
+        eprintln!("Remote name contains invalid characters");
+        exit(1);
+    }
+
+    if !check_for_invalid_characters(branch) {
+        eprintln!("Branch name contains invalid characters");
+        exit(1);
+    }
 
     update_commit_push(commit_message, remote, branch)
-    
+}
+
+
+fn check_for_invalid_characters(name:&str) -> bool {
+    if name.contains(|c:char|  !(c.is_alphanumeric() || c == '-' || c == '_')) {
+        return false
+    }
+    true
 }
